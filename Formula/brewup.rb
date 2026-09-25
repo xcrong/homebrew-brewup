e650aacd90fc921f64171af8b985a468621d8cbc
xcrong <hi@xcrong.me>
2026-09-25 09:05:41 +0800

    Add the brewup formula for release v0.2.1
    Install the published macOS and Linux x86_64 binaries from the brewup release assets.

---
+++ Formula/brewup.rb
@@ -0,0 +1,37 @@
+class Brewup < Formula
+  desc "One-command Homebrew update, upgrade, and cleanup"
+  homepage "https://github.com/xcrong/brewup"
+  version "0.2.1"
+  license "MIT"
+
+  livecheck do
+    url :homepage
+    strategy :github_latest
+  end
+
+  on_macos do
+    on_arm do
+      url "https://github.com/xcrong/brewup/releases/download/v0.2.1/brewup-aarch64-apple-darwin.tar.gz"
+      sha256 "53d26a34769204964d9ede5042e81ffc538d37a0a26197950b0d12248480afa0"
+    end
+    on_intel do
+      url "https://github.com/xcrong/brewup/releases/download/v0.2.1/brewup-x86_64-apple-darwin.tar.gz"
+      sha256 "04daa41291b05dc52c94ec7fbb5d8a8a0c979dab8a621c1a89aba918ebe7a1b5"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/xcrong/brewup/releases/download/v0.2.1/brewup-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d65fb771ca4614c3f613aa96c70b24c82f62a065b383cb19bbb26bcca12fe8d7"
+    end
+  end
+
+  def install
+    bin.install "brewup"
+  end
+
+  test do
+    assert_match "brewup", shell_output("#{bin}/brewup --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew tap for brewup
+
+```bash
+brew tap xcrong/brewup
+brew install brewup
+```
+
+Formula version tracks [brewup releases](https://github.com/xcrong/brewup/releases).
